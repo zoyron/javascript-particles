f6cab6322b4d219223ca79da877ad8b7b836b067
zoyron <sgrarora3@gmail.com>
2020-08-10 20:30:50 +0530

    delayed for later on

---
+++ project1/script.js
@@ -0,0 +1,18 @@
+// const canvas = document.getElementById("canvas1");
+// const ctx = canvas.getContext('2d');
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// let particleArray = [];
+
+// // handle mouse interaction. Will change this later to a single invisible particle or a transparent glassy particle
+
+// const mouse = {
+//   x:null,
+//   y:null,
+//   radius:37.5;
+// };
+
+// window.addEventListener("mousemove", (event) => {
+//   mouse.x = event.x;
+//   mouse.y = event.y;
+// });
